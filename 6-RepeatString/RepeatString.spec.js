a0b20950d16206142877a78ab356e274da84ee00
TheDamrek <kermadabdenour73@gmail.com>
2026-06-30 10:43:15 +0100

    sixth one down, the text files are useless though

---
+++ 6-RepeatString/RepeatString.spec.js
@@ -0,0 +1,31 @@
+const repeatString = require('./RepeatString')
+
+describe('RepeatString', () => {
+  test('repeats the string', () => {
+    expect(repeatString('hey', 3)).toEqual('heyheyhey');
+  });
+  test('repeats the string many times', () => {
+    expect(repeatString('hello', 10)).toEqual('hellohellohellohellohellohellohellohellohellohello');
+  });
+  test('repeats the string 1 time', () => {
+    expect(repeatString('hi', 1)).toEqual('hi');
+  });
+  test('repeats the string 0 times', () => {
+    expect(repeatString('bye', 0)).toEqual('');
+  });
+  test('handles any non-negative integer for repeat count', function () {
+    const number = Math.floor(Math.random() * 1000);
+    expect(repeatString('hey', number)).toBe('hey'.repeat(number));
+  });
+  test('does not use the built-in String repeat method', () => {
+    jest.spyOn(String.prototype, 'repeat').mockName('Built-in String repeat method');
+    repeatString("don't use the built-in repeat method!", 1);
+    expect(String.prototype.repeat).not.toHaveBeenCalled();
+  });
+  test('returns ERROR with negative numbers', () => {
+    expect(repeatString('goodbye', -1)).toEqual('ERROR');
+  });
+  test('works with blank strings', () => {
+    expect(repeatString('', 10)).toEqual('');
+  });
+});
