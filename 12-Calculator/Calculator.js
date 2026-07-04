1db98250c619e8784cdcd92b0cddb7f1847c30d3
TheDamrek <kermadabdenour73@gmail.com>
2026-07-04 17:54:02 +0100

    i'll continue in a moment

---
+++ 12-Calculator/Calculator.js
@@ -0,0 +1,32 @@
+const add = function(a, b) {
+  return a + b;
+};
+
+const subtract = function(a, b) {
+  return a - b;
+};
+
+const sum = function(numbers) {
+	let 
+};
+
+const multiply = function() {
+
+};
+
+const power = function() {
+	
+};
+
+const factorial = function() {
+	
+};
+
+module.exports = {
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial
+};
